9e8a77eb11aef096adfa23813e1529f5b83e95ce
ravindra-rao <ravindra.r1992@gmail.com>
2019-09-19 02:39:23 -0400

    Modified print statement in One.java

--- src/One.java
+++ src/One.java
@@ -2,7 +2,7 @@
 public class One {
 
 	public static void main(String[] args) {
-		System.out.println("project-two Class:One");
+		System.out.println("Current location : project-two Class:One");
 
 	}
 }
