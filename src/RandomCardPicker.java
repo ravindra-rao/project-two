8e3ea865e821356e194bb96ac3b566fadfa43143
ravindra-rao <ravindra.r1992@gmail.com>
2019-09-24 21:56:19 -0400

    Added RandomCardPicker game

---
+++ src/RandomCardPicker.java
@@ -0,0 +1,24 @@
+public class RandomCardPicker {
+	public static void main(String[] args) {
+		int iCardNumbers;
+		int iCardShapes;
+		String sCardNumber;
+		String sCardShape;
+
+		iCardNumbers = (int) (1 + (Math.random() * 13));
+		iCardShapes = (int) (1 + (Math.random() * 4));
+		
+		if(iCardNumbers==1) sCardNumber = "Ace";
+		else if(iCardNumbers==11) sCardNumber = "Jack";
+		else if(iCardNumbers==12) sCardNumber = "Queen";
+		else if(iCardNumbers==13) sCardNumber = "King";
+		else sCardNumber = Integer.toString(iCardNumbers); //default case
+		
+		if(iCardShapes==1) sCardShape = "Clubs";
+		else if(iCardShapes==2) sCardShape = "Diamonds";
+		else if(iCardShapes==3) sCardShape = "Hearts";
+		else sCardShape = "Spades";
+		
+		System.out.println("You picked the " + sCardNumber + " of " + sCardShape);
+	}
+}
