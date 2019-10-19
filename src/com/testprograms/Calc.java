b5d644865fee246b47046f23f138d6658e7b494c
Ravindra Rao <ravindra.r1992@gmail.com>
2019-10-18 21:19:26 -0400

    Added Calc.java - java command line calculator

---
+++ src/com/testprograms/Calc.java
@@ -0,0 +1,82 @@
+package com.testprograms;
+
+import java.util.Scanner;
+
+public class Calc {
+	public static String[] sDelimiters = { "+", "-", "*", "/", "%" };
+	public static String sEnteredDelimiter = "";
+	public static String[] sNumbers;
+	public static int iNum1, iNum2;
+
+	public static void main(String[] args) {
+		parseInput(getInput());
+		int result = performOperation();
+		System.out.println(result);
+
+	}
+
+	public static String getInput() {
+//		System.out.println("Enter operand1*operator*operand2");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.next();
+		scanner.close();
+		return input;
+	}
+
+	public static void parseInput(String input) {
+		for (String sDelimiter : sDelimiters) {
+			if (input.contains(sDelimiter))
+				sEnteredDelimiter = sDelimiter;
+		}
+
+		sNumbers = input.split("\\" + sEnteredDelimiter);
+		iNum1 = Integer.parseInt(sNumbers[0]);
+		iNum2 = Integer.parseInt(sNumbers[1]);
+	}
+
+	public static int performOperation() {
+		int result = 0;
+		switch (sEnteredDelimiter) {
+		case "+":
+			result = sum(iNum1, iNum2);
+			break;
+		case "-":
+			result = difference(iNum1, iNum2);
+			break;
+		case "*":
+			result = product(iNum1, iNum2);
+			break;
+		case "/":
+			result = dividend(iNum1, iNum2);
+			break;
+		case "%":
+			result = remainder(iNum1, iNum2);
+			break;
+		default:
+			System.out.println("Invalid operator entered");
+		}
+		return result;
+
+	}
+
+	public static int sum(int iNum1, int iNum2) {
+		return iNum1 + iNum2;
+	}
+
+	public static int difference(int iNum1, int iNum2) {
+		return iNum1 - iNum2;
+	}
+
+	public static int product(int iNum1, int iNum2) {
+		return iNum1 * iNum2;
+	}
+
+	public static int dividend(int iNum1, int iNum2) {
+		return iNum1 / iNum2;
+	}
+
+	public static int remainder(int iNum1, int iNum2) {
+		return iNum1 % iNum2;
+	}
+
+}
--- src/com/testprograms/StringExample.java
+++ src/com/testprograms/StringExample.java
@@ -78,8 +78,27 @@ public class StringExample {
 		//testStringConcatenationByConcatMethod();
 		//testSubstring();
 		testStringClassMethods();
+
+
 		
 
+	}
+	public static String reverseCase(String text)
+	{
+	    char[] chars = text.toCharArray();
+	    for (int i = 0; i < chars.length; i++)
+	    {
+	        char c = chars[i];
+	        if (Character.isUpperCase(c))
+	        {
+	            chars[i] = Character.toLowerCase(c);
+	        }
+	        else if (Character.isLowerCase(c))
+	        {
+	            chars[i] = Character.toUpperCase(c);
+	        }
+	    }
+	    return new String(chars);
 	}
 	public static void testStringComparisonByEqualsOperator() {
 		//==operator will compare the references and not the actual values of the string objects
